2647cee72e2f07498d71770e904e3d75181506dc
Zelto Lira <you@example.com>
2023-01-23 12:58:39 -0300

    inicia o projeto

---
+++ src/main/java/br/com/petz/clientepet2/ClientePet2Application.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePet2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petz/clientepet2/ClientePet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
